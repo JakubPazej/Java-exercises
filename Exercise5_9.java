5a9120e73e993341b9ad4cbe32f1bb1a09dccd62
MiestnyLovecZien <48026495+MiestnyLovecZien@users.noreply.github.com>
2019-09-23 14:16:38 +0100

    Add files via upload

--- Exercise5_9.java
+++ Exercise5_9.java
@@ -1,10 +1,10 @@
 public class Exercise5_9{
   public static double footToMeter(double foot){
-    double conversion = foot*3.279;
+    double conversion = foot*0.305;
     return conversion;
   }
   public static double meterToFoot(double meter){
-    double conversion = meter*0.305;
+    double conversion = meter*3.279;
     return conversion;
   }
 }
--- Exercise5_9_testprogram.java
+++ Exercise5_9_testprogram.java
@@ -1,15 +1,29 @@
+import java.util.*;
+import java.util.NumberFormat;
 public class Exercise5_9_testprogram{
     public static void main(String[] args){
         Exercise5_9 a = new Exercise5_9();
-        double[][] table = new double[10][4];
-    
-        //for(int i=1; i<=10; i++){
-            table[0] = new double[]{1,a.footToMeter(1),(20+(1*5)),a.meterToFoot(20+(1*5))};
-        //}
-        System.out.println(table[0]);
-        //for(double[] x : table){
-            //System.out.format("%-8s%-8s%-8s%-8s\n", x);
-            //System.out.println(x);
-        //}
+        double[][] table = new double[10][5];
+        double i = 1;
+        System.out.println("Feet    Meters    Meters    Feet");
+        System.out.println("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾");
+        NumberFormat formatter = new DecimalFormat("#0.000");
+        for(int row=0; row<10; row++){
+           table[row][0] = i;
+           table[row][1] = a.footToMeter(i);
+           table[row][2] = (15+(i*5));
+           table[row][3] = a.meterToFoot(15+(i*5));
+           i++;
+        }
+        
+        String[][] idk = new String[table.length][table[0].length];
+        for(int b=0; b<10; b++){
+            for(int c=0; c<4; c++){
+                idk[b][c] = String.valueOf(table[b][c]);
+            }
+        }
+        for(String[] string : idk){
+            System.out.format("%5s%10s%10s%10s\n", string);
+        }
     }
 }
