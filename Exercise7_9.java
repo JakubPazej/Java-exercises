9dc7050dd9afae5db9fdd5058200b7545de68406
MiestnyLovecZien <48026495+MiestnyLovecZien@users.noreply.github.com>
2019-09-25 20:29:56 +0100

    Add files via upload

--- Exercise7_9.java
+++ Exercise7_9.java
@@ -1,23 +1,68 @@
 import java.util.Scanner;
 public class Exercise7_9{
+  static String[][] board = new String[3][3];
   public static void main(String[] args){
-    String[][] board = new String[3][3]
     Scanner scanner = new Scanner (System.in);
+    
+    for(int rounds = 0; rounds<9; rounds++){
+        for(int i=1; i<2;i++){
+            System.out.print("Enter a row (0, 1 or 2) for player X: ");
+            int pxRow = scanner.nextInt();
 
-    System.out.print("Enter a row (0, 1 or 2) for player X: ");
-    String pxRow = scanner.next();
-    int x = Integer.parseInt(pxRow);
+            System.out.print("Enter a column (0, 1 or 2) for player X: ");
+            int pxCol = scanner.nextInt();
+            if(board[pxRow][pxCol]!=null){
+                System.out.println("Spot already taken.");
+                i--;
+            }else{
+                board[pxRow][pxCol] = "X";
+                table();
+            }
+        }
+        for(int x=1; x<2; x++){
+            System.out.print("Enter a row (0, 1 or 2) for player O: ");
+            int poRow = scanner.nextInt();
 
-    System.out.print("Enter a column (0, 1 or 2) for player X: ");
-    String pxCol = scanner.next();
-    int x1 = Integer.parseInt(pxCol);
-
-    System.out.print("Enter a row (0, 1 or 2) for player O: ");
-    String poRow = scanner.next();
-    int o = Integer.parseInt(poRow);
-
-    System.out.print("Enter a column (0, 1 or 2) for player O: ");
-    String poCol = scanner.next();
-    int o1 = Integer.parseInt(poCol);
+            System.out.print("Enter a column (0, 1 or 2) for player O: ");
+            int poCol = scanner.nextInt();
+            if(board[poRow][poCol]!=null){
+                System.out.println("Spot already taken.");
+                x--;
+            }else{
+                board[poRow][poCol] = "O";
+                table();
+            }
+        }
+    }
+  }
+  private static void table(){
+      for(int row=0; row<board.length;row++){
+          for(int col=0; col<board[0].length;col++){
+              if(board[row][col]==null){
+                  board[row][col]=" ";
+              }
+          }
+      }
+      System.out.println("-------");
+      System.out.println("|"+board[0][0]+"|"+board[0][1]+"|"+board[0][2]+"|");
+      System.out.println("-------");
+      System.out.println("|"+board[1][0]+"|"+board[1][1]+"|"+board[1][2]+"|");
+      System.out.println("-------");
+      System.out.println("|"+board[2][0]+"|"+board[2][1]+"|"+board[2][2]+"|");
+      System.out.println("-------");
+  }
+  private static boolean checkWinner(){
+      if(((board[0][0])==(board[0][1])&&(board[0][1])==(board[0][2]))||
+         ((board[1][0])==(board[1][1])&&(board[1][1])==(board[1][2]))||
+         ((board[2][0])==(board[2][1])&&(board[2][1])==(board[2][2]))|| //vertical win       
+         ((board[0][0])==(board[1][0])&&(board[1][0])==(board[2][0]))||
+         ((board[0][1])==(board[1][1])&&(board[1][1])==(board[2][1]))||
+         ((board[0][3])==(board[1][3])&&(board[1][3])==(board[2][3]))|| //horizontal      
+         ((board[0][0])==(board[1][1])&&(board[1][1])==(board[2][2]))||
+         ((board[0][2])==(board[1][1])&&(board[1][1])==(board[2][0])))  //cross win
+      {
+          return true;
+      }
+      return false;
   }
 }
